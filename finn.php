f5dd0654611213f6092a722915aa568f895a3bff
Cristian Metz <cristian@mobilityps.com>
2021-03-22 11:46:03 -0300

    finn

---
+++ finn.php
@@ -0,0 +1,96 @@
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+<div class='col-md-12' style='height:100%; background-color:grey'>
+    <div class='row' style='height:100%;display: -webkit-box;
+  -webkit-box-pack: center;
+  -webkit-box-align: center;'>
+        <div style='height:100%; width:18em; '>
+            <div class='row' style='height:100%;'>
+                <div class='col-md-2'>
+                    <div class='row' style='height:22em;'>
+                        <div style='width:50%;margin-top:12em;'>
+                            <div style='height:20%; background-color:#3189A7;border-top-left-radius:5em;'>
+                            </div>
+                            <div style='height:80%;background-color:#FFD2C0'>
+                            </div>
+                        </div>
+                        <div style='width:50%;margin-top:10em;'>
+                            <div style='height:50%;width:100%;'>
+                                <div style='height:30%;width:100%;background-color:#9EE27F;border-top-left-radius:5em;'>
+                                </div>
+                                <div style='height:70%;width:100%;background-color:#3A7B1C;border-bottom-left-radius:5em;'></div>
+                            </div>
+                            <div style='height:50%;width:100%'></div>
+                        </div>
+                    </div>
+                </div>
+                <div class='col-md-8'>
+                    <div class='row' style='height:2em;'></div>
+                    <div class='row' style='height:10em; '>
+                        <div class='col-md-2' style='background-color:white;border-top-left-radius:1em;border-top-right-radius:1em;'></div>
+                        <div class='col-md-8'>
+                            <div class='row' style="height:20%;">
+                            </div>
+                            <div class='row' style="height:80%;background-color:white">
+                                <div class='col-md-12' style='border-radius:70em;background-color:#FFD2C0; margin-top:10%;margin-bottom:10%;'>
+                                                        
+                                </div>
+                            </div>
+                        </div>
+                        <div class='col-md-2' style='background-color:white;border-top-left-radius:1em;border-top-right-radius:1em;'></div>
+                    </div>
+                    <div class='row' style='height:15em; background-color:#3189A7'></div>
+                    <div class='row' style='height:20em; '>
+                        <div class='col-md-2'>
+                            <div class='row' style='height:30%;background-color:#326CA8; '>
+                            </div>
+                            <div class='row' style='height:70%;'>
+                                <div class='col-md-12'>
+                                    <div class='row' style='height:70%; background-color:#FFD2C0;'></div>
+                                    <div class='row' style='height:10%; border-radius:50em;background-color:white;margin-left:-1.8em;margin-right:-1.8em'></div>
+                                    <div class='row' style='height:20%; background-color:black;margin-left:-1.1em;margin-right:-1.1em'></div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class='col-md-8'>
+                            <div class='row' style='height:10%;background-color:#326CA8;'>
+                            </div>
+                            <div class='row' style='height:80%;'>
+                            </div>
+                        </div>
+                        <div class='col-md-2'>
+                            <div class='row' style='height:30%;background-color:#326CA8; '>
+                            </div>
+                            <div class='row' style='height:70%;'>
+                                <div class='col-md-12'>
+                                    <div class='row' style='height:70%; background-color:#FFD2C0;'></div>
+                                    <div class='row' style='height:10%; border-radius:50em;background-color:white;margin-left:-1.8em;margin-right:-1.8em'></div>
+                                    <div class='row' style='height:20%; background-color:black;margin-left:-1.1em;margin-right:-1.1em'></div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class='col-md-2'>
+                    <div class='row' style='height:22em;'>
+                        <div style='width:50%;margin-top:10em;'>
+                            <div style='height:50%;width:100%;'>
+                                <div style='height:30%;width:100%;background-color:#9EE27F;border-top-right-radius:5em;'>
+                                </div>
+                                <div style='height:70%;width:100%;background-color:#3A7B1C;border-bottom-right-radius:5em;'></div>
+                            </div>
+                            <div style='height:50%;width:100%'></div>
+                        </div>
+                        <div style='width:50%;margin-top:12em;'>
+                            <div style='height:20%; background-color:#3189A7;border-top-right-radius:5em;'>
+                            </div>
+                            <div style='height:80%;background-color:#FFD2C0'>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+</div>
+</div>
